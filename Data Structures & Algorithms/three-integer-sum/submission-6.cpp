a01162e669220ceba8afe267067b7350537673bb
Luka Nikolic <96079642+nluka4@users.noreply.github.com>
2026-04-04 17:21:19 +0200

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>resenje;
+        int sum = 0;
+        
+        for(int i = 0; i < nums.size();i++){
+            for(int j = i+1; j < nums.size();j++){
+                for(int k = j+1; k < nums.size();k++){
+                    sum=nums[i]+nums[j]+nums[k];
+                    if(sum == 0){
+                        resenje.push_back({nums[i],nums[j],nums[k]});
+                    }
+                    sum-=nums[k];
+                }
+                sum-=nums[j];
+            }
+        sum = 0;
+    }
+
+    
+
+    for (int i = 0; i < resenje.size(); i++) {
+        sort(resenje[i].begin(), resenje[i].end());
+        for (int j = i + 1; j < resenje.size(); ) {
+            sort(resenje[j].begin(), resenje[j].end());
+            if (resenje[i] == resenje[j]) {
+                resenje.erase(resenje.begin() + j);
+            } else {
+                j++;
+            }
+        }
+    }
+
+        return resenje;
+    }
+};
